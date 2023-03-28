67b8af07895cde4c555d1e48765481111af14e5b
Sohel-Developer <sohel.webdevoloper@gmail.com>
2023-03-28 12:47:50 +0600

    Cart All problem sloved

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,10 +3,13 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   let price = 0;
   let shipping = 0;
+  let quantity = 0;
 
   for (const product of cart) {
+    product.quantity = product.quantity || 1;
     shipping = shipping + product.shipping;
-    price = price + product.price;
+    price = price + product.price * product.quantity;
+    quantity = quantity + product.quantity;
   }
 
   const tax = (price * 7) / 100;
@@ -17,11 +20,11 @@ const Cart = ({ cart }) => {
     <div className="cart">
       <div className="cart-info">
         <h2>Order Summary</h2>
-        <p>Selected Item: {cart.length}</p>
-        <p>Total Price: ${price}</p>
-        <p>Total Shipping Charge: ${shipping}</p>
-        <p>Tax: ${tax.toFixed(2)}</p>
-        <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
+        <p>Selected Item: {quantity}</p>
+        <p>Total Price: $ {price}</p>
+        <p>Total Shipping Charge: $ {shipping}</p>
+        <p>Tax: $ {tax.toFixed(2)}</p>
+        <h3>Grand Total: $ {grandTotal.toFixed(2)}</h3>
       </div>
       <div className="btn">
         <button className="clear-btn">Clear Cart</button>
