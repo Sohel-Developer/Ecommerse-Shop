a648c7cca837370ecba58068df93c57f1165f797
Sohel-Developer <sohel.webdevoloper@gmail.com>
2023-04-05 14:22:22 +0600

    Order page development 80% done

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
   const cart = useLoaderData();
   return (
     <div className="shop-container container">
-      <div className="products-container"> save {cart.length} -- Orders</div>
+      <div className="">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
+      </div>
       <div className="cart-container">
         <Cart cart={cart} />
       </div>
--- src/Components/ReviewItem/ReviewItem.js
+++ src/Components/ReviewItem/ReviewItem.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-const ReviewItem = () => {
-    return (
+const ReviewItem = ({ product }) => {
+  const { name, quantity, img, price, shipping } = product;
+  return (
+    <div className="review__item">
+      <div className="review__detail">
+        <img src={img} alt="" />
         <div>
-            <h1>Review Single Item</h1>
+          <h3>{name}</h3>
+          <p>
+            Price : <span>$ {price}</span>
+          </p>
+          <p>
+            Shipping Charge : <span>$ {shipping}</span>
+          </p>
+          <p>Quantity : {quantity} </p>
         </div>
-    );
+      </div>
+
+      <div>
+        <span>
+          <FontAwesomeIcon className="deleteIcon" icon={faTrashCan} />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewItem;
